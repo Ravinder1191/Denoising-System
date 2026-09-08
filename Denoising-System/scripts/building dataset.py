8fd2551a709cd09dbc3bbaeebe9158f635afb735
RAVINDERJEET SINGH <200975566+Ravinder1191@users.noreply.github.com>
2026-09-08 18:19:30 +0530

    Delete Denoising-System directory

--- Denoising-System/scripts/building dataset.py
+++
@@ -1,6 +0,0 @@
-from extract_waves import extract_clean_speech
-
-extract_clean_speech(
-    download_path="audio data",
-    out_clean_dir=r"C:\Users\Ravin\PycharmProjects\vokie\data\clean_raw_for_augment"
-)
